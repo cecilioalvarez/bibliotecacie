8b44fb256a9e01622a2da7f8690eeea3279ffba0
cie <cie@cie-VirtualBox>
2025-01-27 11:47:04 +0100

    Correcciones Cecilio

--- bibliotecacie/src/main/java/es/cie/repositories/LibroRepository.java
+++ bibliotecacie/src/main/java/es/cie/repositories/LibroRepository.java
@@ -8,9 +8,9 @@ import es.cie.negocio.Libro;
 public interface LibroRepository {
 	
 	List<Libro> buscarTodos ();
-	List<Libro> porAutor(Libro libro);
-	List<Libro> porTitulo (Libro libro);
-	List<Libro> porOrden (Libro orden);
+	List<Libro> porAutor(String libro);
+	List<Libro> porTitulo (String libro);
+	List<Libro> porOrden (String orden);
 	void insertar (Libro libro);
 	
 	
