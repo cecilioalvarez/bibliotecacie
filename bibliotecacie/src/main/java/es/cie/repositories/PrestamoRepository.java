d8ddbdb5e742ec844139238a76d9f6544316f292
cie <cie@cie-VirtualBox>
2025-01-27 10:16:41 +0100

    interfaz PrestamoREpository

---
+++ bibliotecacie/src/main/java/es/cie/repositories/PrestamoRepository.java
@@ -0,0 +1,13 @@
+package es.cie.repositories;
+
+import java.util.List;
+
+import es.cie.negocio.Prestamo;
+
+public interface PrestamoRepository {
+
+	List<Prestamo> buscarTodos();
+	void insertar(Prestamo prestamo);
+	void borrar(Prestamo prestamo);
+	List<Prestamo> buscarTodosOrdenados(String orden);
+}
