a38ece7e5f7da689f0c0b0e99417b43d66de27d2
cie <cie@cie-VirtualBox>
2025-01-27 10:28:43 +0100

    Ejemplar + Servlet

--- bibliotecacie/src/main/java/es/cie/controladores/EjemplarServlet.java
+++ bibliotecacie/src/main/java/es/cie/controladores/EjemplarServlet.java
@@ -10,79 +10,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import es.ascender.biblioteca.negocio.Socio;
-import es.ascender.biblioteca.repositories.SocioRepository;
-import es.ascender.biblioteca.repositories.jdbc.SocioRepositoryJDBC;
+import es.cie.negocio.Ejemplar;
+import es.cie.repositories.EjemplarRepository;
+import es.cie.repositories.jdbc.EjemplarRepositoryJDBC;
 
-
-@WebServlet("/SociosServlet")
+@WebServlet("/EjemplarServlet")
 public class EjemplarServlet extends HttpServlet {
-	
 	private static final long serialVersionUID = 1L;
-	
-	SocioRepository repo = new SocioRepositoryJDBC();
-	List<Socio> lista = null;
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		if (request.getParameter("comando")==null ) {
-		
-		if (request.getParameter("orden") != null) {
-			lista = repo.buscarTodosOrdenados(request.getParameter("orden"));
+	EjemplarRepository repo = new EjemplarRepositoryJDBC();
+	List<Ejemplar> lista = null;
 
-		} else {
-			lista = repo.buscarTodos();
-		}
-		request.setAttribute("lista",lista);
-		RequestDispatcher despachador= request.getRequestDispatcher("listasociosJDBC.jsp");
-		despachador.forward(request, response);
-		
-	} else {
-		String comando=request.getParameter("comando");
-		if (comando.equals("formulariosocio")) {
-			
-		RequestDispatcher despachador= request.getRequestDispatcher("formulariosocio.html");
-		despachador.forward(request,response);
-		
-		} else if (comando.equals("salvarsocio")) {
-			
-			String dni=request.getParameter("dni");
-			String nombre=request.getParameter("nombre");
-			String apellidos=request.getParameter("apellidos");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-			Socio s= new Socio(dni,nombre,apellidos);
-			SocioRepository repo= new SocioRepositoryJDBC();
-			repo.insertar(s);
-			lista = repo.buscarTodos();
-			
-			
-			request.setAttribute("lista",lista);
-			RequestDispatcher despachador= request.getRequestDispatcher("listasociosJDBC.jsp");
-			despachador.forward(request, response);
-			
-		}	else if (comando.equals("borrarsocio")) {
-			
-			String dni=request.getParameter("dni");
+		lista = repo.buscarTodos();
 
-			Socio s= new Socio(dni);
-			SocioRepository repo= new SocioRepositoryJDBC();
-			repo.borrar(s);
-			lista = repo.buscarTodos();
-			
-
-			request.setAttribute("lista",lista);
-			RequestDispatcher despachador= request.getRequestDispatcher("listasociosJDBC.jsp");
-			despachador.forward(request, response);
-		}
-		
+		request.setAttribute("lista", lista);
+		RequestDispatcher dp = request.getRequestDispatcher("listaejemplarjdbc.jsp");
+		dp.forward(request, response);
 	}
-		
-	}
-	
-	
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
