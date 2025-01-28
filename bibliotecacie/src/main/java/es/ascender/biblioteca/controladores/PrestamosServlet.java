5929d78bd7f07b5d14717be538698c337f4a59d5
cie <cie@cie-VirtualBox>
2025-01-28 10:53:28 +0100

    intento de sacar lineas fallido

--- bibliotecacie/src/main/java/es/ascender/biblioteca/controladores/PrestamosServlet.java
+++ bibliotecacie/src/main/java/es/ascender/biblioteca/controladores/PrestamosServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
@@ -17,6 +16,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import es.cie.negocio.LineaPrestamo;
 import es.cie.negocio.Prestamo;
 import es.cie.repositories.PrestamoRepository;
 import es.cie.repositories.jdbc.PrestamoRepositoryJDBC;
@@ -43,57 +43,75 @@ public class PrestamosServlet extends HttpServlet {
 			request.setAttribute("lista", lista);
 			RequestDispatcher despachador = request.getRequestDispatcher("listaprestamosjdbc.jsp");
 			despachador.forward(request, response);
-			
-		}else{
-				String comando = request.getParameter("comando");
-				
-				if (comando.equals("formularioprestamo")) {
-					RequestDispatcher despachador=request.getRequestDispatcher("formularioprestamo.html");
-					despachador.forward(request, response);
-					
-				}else if (comando.equals("salvarprestamo")) {
-					int identificador=Integer.parseInt(request.getParameter("identificador"));
-					String fechaTexto= request.getParameter("fecha");
-					Calendar calendario= Calendar.getInstance();
-					DateFormat formateador= new SimpleDateFormat ("yyyy-MM-dd");
-					Date fecha=null;
-					try {
-						fecha = formateador.parse(fechaTexto);
-					} catch (ParseException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-					calendario.setTime(fecha);
-					
-					Prestamo p=new Prestamo(identificador, fecha.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
-					PrestamoRepository repo=new PrestamoRepositoryJDBC();
-					repo.insertar(p);
-					
-					lista = repo.buscarTodos();
-					
-					request.setAttribute("lista", lista);
-					RequestDispatcher despachador = request.getRequestDispatcher("listaprestamosjdbc.jsp");
-					despachador.forward(request, response);
-				}else if(comando.equals("borrarprestamo")) {
-					int identificador=Integer.parseInt(request.getParameter("identificador"));
 
-					Prestamo p=new Prestamo(identificador);
-					PrestamoRepository repo=new PrestamoRepositoryJDBC();
-					repo.borrar(p);
-					
-					lista = repo.buscarTodos();
-					
-					request.setAttribute("lista", lista);
-					RequestDispatcher despachador = request.getRequestDispatcher("listaprestamosjdbc.jsp");
-					despachador.forward(request, response);
+		} else {
+			String comando = request.getParameter("comando");
+
+			if (comando.equals("formularioprestamo")) {
+				RequestDispatcher despachador = request.getRequestDispatcher("formularioprestamo.html");
+				despachador.forward(request, response);
+
+			} else if (comando.equals("salvarprestamo")) {
+				int identificador = Integer.parseInt(request.getParameter("identificador"));
+				String fechaTexto = request.getParameter("fecha");
+				Calendar calendario = Calendar.getInstance();
+				DateFormat formateador = new SimpleDateFormat("yyyy-MM-dd");
+				Date fecha = null;
+				try {
+					fecha = formateador.parse(fechaTexto);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-		}
-	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+				calendario.setTime(fecha);
+
+				Prestamo p = new Prestamo(identificador,
+						fecha.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+				PrestamoRepository repo = new PrestamoRepositoryJDBC();
+				repo.insertar(p);
+
+				lista = repo.buscarTodos();
+
+				request.setAttribute("lista", lista);
+				RequestDispatcher despachador = request.getRequestDispatcher("listaprestamosjdbc.jsp");
+				despachador.forward(request, response);
+				} 
+			else if (comando.equals("borrarprestamo")) {
+				
+				int identificador = Integer.parseInt(request.getParameter("identificador"));
+
+				Prestamo p = new Prestamo(identificador);
+				PrestamoRepository repo = new PrestamoRepositoryJDBC();
+				repo.borrar(p);
+
+				lista = repo.buscarTodos();
+
+				request.setAttribute("lista", lista);
+				RequestDispatcher despachador = request.getRequestDispatcher("listaprestamosjdbc.jsp");
+				despachador.forward(request, response);
+			
+				
+				
+			}
+		}
+	List<LineaPrestamo> lista1=null;
+	
+	if (request.getParameter("comando") == null) {
+
+		if (request.getParameter("buscarLineas") != null) {
+			
+			lista1 = repo.buscarLineas(Integer.parseInt(request.getParameter("identificador")));
+
+		} else {
+
+		}
+		request.setAttribute("lista", lista1);
+		RequestDispatcher despachador = request.getRequestDispatcher("listalineaprestamosjdbc.jsp");
+		despachador.forward(request, response);
+
 	
 	}
 
+}
 }
